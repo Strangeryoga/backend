4b665d2d1d1c0a9116a9305a43412cb147362fdf
Strangeryoga <tejasdoke4@gmail.com>
2023-11-11 00:48:40 +0530

    completed flash

--- flashmessages/app.js
+++ flashmessages/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressSession = require('express-session');
+const flash= require('connect-flash')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'hello boy'
+}));
+
+app.use(flash());
 
 app.use(logger('dev'));
 app.use(express.json());
--- flashmessages/routes/index.js
+++ flashmessages/routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
 });
+
+router.get('/failed', function(req, res) {
+  req.flash('age', 12);
+  req.flash('name', 'tejas')
+  res.send("Done");
+})
+
+router.get('/check', function(req, res) {
+  console.log(req.flash('age'), req.flash('name'));
+  res.send("check it in terminal")
+})
+
+
 
 module.exports = router;
